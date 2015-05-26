37acc377e1d876f11964f9190cb73d376f60ef97
Reed Hedges <reed@interreality.org>
2015-05-25 22:07:11 -0400

    many fixes to get kinect and open cv images into arvideo

--- ArVideoOpenCV.h
+++ ArVideoOpenCV.h
@@ -49,27 +49,56 @@ public:
   {
   }
 
-  /** The cv::Mat::convertTo() method is used. @a scale is passed as @a alpha.
-    Normally a cv::Mat object is provided for @a input but cv::InputArray type
-    allows other OpenCV types to also be used.
+  /** The cv::Mat::convertTo() method is used. 
+    @param input Normally a cv::Mat object is provided for @a input but cv::InputArray type
+    allows other OpenCV types to also be used. 
+    @param pixelscale Use to scale pixel values to unsigned char values (0, 255)
+used by ArVideo.   For example, to convert floating point values (0.0, 1.0),
+pass 255.0.  If your image is already in (0,255) pixel values, omit or specify
+1.0.
+    @param convertchannelstorgb If @a input is not in RGB channel
+    format, pass a CV_???2RGB code as @a convertchannelstorgb (this is used with
+    the <tt>cvtColor</tt> OpenCV function).  For example, if
+    the image is in BGR channel order (common in OpenCV), then specify
+    CV_BGR2RGB. IF the image is one channel (greyscale), specify CV_GRAY2RGB. If
+    HSV, specify CV_HSV2RGB. See OpenCV documetation for <tt>cvtColor</tt> for full
+    list of codes and further details:
+    http://docs.opencv.org/modules/imgproc/doc/miscellaneous_transformations.html#cvtcolor
   */
-  virtual bool updateVideoDataCopy(cv::InputArray input, double scale = 1)
+  virtual bool updateVideoDataCopy(cv::InputArray input, double pixelscale = 1.0, int convertchannelstorgb = 0)
   {
     lock();
-    input.getMat().convertTo(myCvData, CV_8UC3, scale);
+    cv::Mat tmp;
+    input.getMat().convertTo(tmp, CV_8U, pixelscale);
+    if(convertchannelstorgb > 0)
+      cvtColor(tmp, myCvData, convertchannelstorgb);
+    // XXX BUG this is turning stuff blue?
     myWidth = myCvData.cols;
     myHeight = myCvData.rows;
+   // printf("ArVideoOpenCV: update %dx%d\n  scaled %f\n", myWidth, myHeight, scale);
     assert(input.getMat().cols == myCvData.cols);
     assert(input.getMat().rows == myCvData.rows);
     if(myWidth == -1 || myHeight == -1)
       return false;
+    myData = myCvData.ptr();
     unlock();
     updated();
+
+    /*
+    lock();
+    FILE *f = fopen("test.ppm", "w");
+    assert(f);
+    fprintf(f, "P6\n%d %d 255\n", myWidth, myHeight);
+    fwrite(myData, 3,  myWidth*myHeight, f);
+    fclose(f);
+    unlock();
+    */
+
     return true;
   }
 
   virtual unsigned char *getData() {
-    return myCvData.ptr();
+    return myData;
   }
 
   virtual bool setVideoDataPtr(unsigned char* ptr, int width, int height)
--- kinectArVideoServer.cpp
+++ kinectArVideoServer.cpp
@@ -1,189 +1,206 @@
 /*
-Adept MobileRobots Robotics Interface for Applications (ARIA)
-Copyright (C) 2004-2005 ActivMedia Robotics LLC
-Copyright (C) 2006-2010 MobileRobots Inc.
-Copyright (C) 2011-2015 Adept Technology
+ * This file is part of the OpenKinect Project. http://www.openkinect.org
+ *
+ * Copyright (c) 2011 individual OpenKinect contributors. See the CONTRIB file
+ * for details.
+ *
+ * This code is licensed to you under the terms of the Apache License, version
+ * 2.0, or, at your option, the terms of the GNU General Public License,
+ * version 2.0. See the APACHE20 and GPL2 files for the text of the licenses,
+ * or the following URLs:
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * If you redistribute this file in source form, modified or unmodified, you
+ * may:
+ *   1) Leave this header intact and distribute it under the same terms,
+ *      accompanying it with the APACHE20 and GPL20 files, or
+ *   2) Delete the Apache 2.0 clause and accompany it with the GPL2 file, or
+ *   3) Delete the GPL v2 clause and accompany it with the APACHE20 file
+ * In all cases you must keep the copyright notice intact and include a copy
+ * of the CONTRIB file.
+ *
+ * Binary distributions must follow the binary distribution requirements of
+ * either License.
+ */
 
-     This program is free software; you can redistribute it and/or modify
-     it under the terms of the GNU General Public License as published by
-     the Free Software Foundation; either version 2 of the License, or
-     (at your option) any later version.
 
-     This program is distributed in the hope that it will be useful,
-     but WITHOUT ANY WARRANTY; without even the implied warranty of
-     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-     GNU General Public License for more details.
-
-     You should have received a copy of the GNU General Public License
-     along with this program; if not, write to the Free Software
-     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-If you wish to redistribute ARIA under different terms, contact 
-Adept MobileRobots for information about a commercial version of ARIA at 
-robots@mobilerobots.com or 
-Adept MobileRobots, 10 Columbia Drive, Amherst, NH 03031; +1-603-881-7960
-*/
-
-//#include "Aria.h"
-//#include "ArSystemStatus.h"
-
-//#include "ArVideo.h"
-//#include "ArVideoConnector.h"
-//#include "ArPTZConnector.h"
-
-//#include "ArServerBase.h"
-//#include "ArServerHandlerCommMonitor.h"
-//#include "ArSystemStatus.h"
-//#include "ArServerInfoStrings.h"
-//#include "ArServerHandlerCommands.h"
-
-//#ifndef WIN32
-//#include "ArServerFileUtils.h"
-//#endif
-
-#include <stdio.h>
+#include <iostream>
+#include <signal.h>
 
 #include <opencv2/opencv.hpp>
+
 #include <libfreenect2/libfreenect2.hpp>
 #include <libfreenect2/frame_listener_impl.h>
-//#include <libfreenect2/threading.h>
+#include <libfreenect2/threading.h>
 
-//#include "ArVideoOpenCV.h"
+#include "Aria.h"
+#include "ArVideo.h"
+#include "ArVideoOpenCV.h"
+#include "ArSystemStatus.h"
 
+bool protonect_shutdown = false;
 
-int main(int argc, char **argv)
+void sigint_handler(int s)
 {
-#if 0
+  protonect_shutdown = true;
+}
+
+libfreenect2::Freenect2Device *freenect_dev = NULL;
+
+void shutdown_app()
+{
+  // TODO: restarting ir stream doesn't work!
+  // TODO: bad things will happen, if frame listeners are freed before dev->stop() :(
+  freenect_dev->stop();
+  freenect_dev->close();
+}
+
+int main(int argc, char *argv[])
+{
+
   Aria::init();
-  ArVideo::init();  
+  ArVideo::init();
 
-  /* To get command line arguments: */
-  ArArgumentParser argParser(&argc, argv);
-  argParser.loadDefaultArguments();
+  Aria::addExitCallback(new ArGlobalFunctor(&shutdown_app));
 
-  /* Used To set up ArNetworking server: */
+  std::string program_path(argv[0]);
+  size_t executable_name_idx = program_path.rfind("Protonect");
+
+  std::string binpath = "/";
+
+  if(executable_name_idx != std::string::npos)
+  {
+    binpath = program_path.substr(0, executable_name_idx);
+  }
+
+
+  /* Open Kinect */
+  libfreenect2::Freenect2 freenect2;
+  libfreenect2::Freenect2Device *dev = freenect2.openDefaultDevice();
+  freenect_dev = dev;
+
+  if(dev == 0)
+  {
+    std::cout << "no device connected or failure opening the default one!" << std::endl;
+    return -1;
+  }
+
+
   ArServerBase server;
-  ArServerSimpleOpener simpleOpener(&argParser);
+  ArServerSimpleOpener openServer(NULL);
+  
 
 
-  /* Parse command line arguments */
-  if (!Aria::parseArgs())
-  {
-    ArLog::log(ArLog::Terse, "kinectArVideoServer: Error parsing program arguments.");
-    Aria::exit(1);
-  }
 
-  if(!argParser.checkHelpAndWarnUnparsed())
-  {
-    Aria::logOptions();
-    Aria::exit(0);
-    return 0;
-  }
+  /* Shutdown cleanly */
+  signal(SIGINT,sigint_handler);
+  protonect_shutdown = false;
 
 
-  /* Set up ArNetworking server with various other services */
+  /* Set up libfreenect listeners, start capturing  */
+
+  //libfreenect2::SyncMultiFrameListener listener(libfreenect2::Frame::Color | libfreenect2::Frame::Ir | libfreenect2::Frame::Depth);
+  libfreenect2::SyncMultiFrameListener listener(libfreenect2::Frame::Color | libfreenect2::Frame::Depth);
+  libfreenect2::FrameMap frames;
+
+  dev->setColorFrameListener(&listener);
+  dev->setIrAndDepthFrameListener(&listener);
+  dev->start();
+
+  std::cout << "device serial: " << dev->getSerialNumber() << std::endl;
+  std::cout << "device firmware: " << dev->getFirmwareVersion() << std::endl;
+
+
+  /* Set up ArNetworking services */
   ArServerHandlerCommands commandsServer(&server);
-
+ 
 #ifndef WIN32
   ArServerFileLister fileLister(&server, ".");
   ArServerFileToClient fileToClient(&server, ".");
   ArServerDeleteFileOnServer deleteFileOnServer(&server, ".");
 #endif
-
+   
   ArServerInfoStrings stringInfoServer(&server);
+
   Aria::getInfoGroup()->addAddStringCallback(stringInfoServer.getAddStringFunctor());
-  ArSystemStatus::startPeriodicUpdate();
+  ArSystemStatus::startPeriodicUpdate(); 
   Aria::getInfoGroup()->addStringDouble(
-    "CPU Use", 10, ArSystemStatus::getCPUPercentFunctor(), "% 4.0f%%");
-//  Aria::getInfoGroup()->addStringUnsignedLong(
-//    "Computer Uptime", 14, ArSystemStatus::getUptimeFunctor());
-//  Aria::getInfoGroup()->addStringUnsignedLong(
-//    "Program Uptime", 14, ArSystemStatus::getProgramUptimeFunctor());
+     "CPU Use", 10, ArSystemStatus::getCPUPercentFunctor(), "% 4.0f%%");
+ //  Aria::getInfoGroup()->addStringUnsignedLong(
+ //    "Computer Uptime", 14, ArSystemStatus::getUptimeFunctor());
+ //  Aria::getInfoGroup()->addStringUnsignedLong(
+ //    "Program Uptime", 14, ArSystemStatus::getProgramUptimeFunctor());
   Aria::getInfoGroup()->addStringInt(
-    "Wireless Link Quality", 9, ArSystemStatus::getWirelessLinkQualityFunctor(), "%d");
+     "Wireless Link Quality", 9, ArSystemStatus::getWirelessLinkQualityFunctor(), "%d");
   Aria::getInfoGroup()->addStringInt(
-    "Wireless Noise", 10, ArSystemStatus::getWirelessLinkNoiseFunctor(), "%d");
+     "Wireless Noise", 10, ArSystemStatus::getWirelessLinkNoiseFunctor(), "%d");
   Aria::getInfoGroup()->addStringInt(
-    "Wireless Signal", 10, ArSystemStatus::getWirelessLinkSignalFunctor(), "%d");
+     "Wireless Signal", 10, ArSystemStatus::getWirelessLinkSignalFunctor(), "%d");
   ArServerHandlerCommMonitor commMonitorServer(&server);
 
 
-  /* Create ArVideo sources and servers for kinect images */
-  ArVideoOpenCV kinectDepthSource("Kinect_Depth"), kinectIRSource("Kinect_IR"), kinectRGBSource("Kinect_RGB");
-  ArVideo::createVideoServer(&server, &kinectDepthSource, "Kinect_Depth", "Kinect_Depth");
-  ArVideo::createVideoServer(&server, &kinectRGBSource, "Kinect_RGB", "Kinect_RGB");
-  ArVideo::createVideoServer(&server, &kinectIRSource, "Kinect_IR", "Kinect_IR");
-#endif
-  
-  /* Set up kinect */
-  libfreenect2::Freenect2 freenect2;
-  libfreenect2::Freenect2Device *kinectDev = freenect2.openDefaultDevice();
 
-  if(kinectDev == 0)
+  ArVideoOpenCV kinectDepthSource("Kinect_Depth");
+  ArVideo::createVideoServer(&server, &kinectDepthSource, "Kinect_Depth", "freenect2|Depth|OpenCV");
+  ArVideoOpenCV kinectRGBSource("Kinect_RGB");
+  ArVideo::createVideoServer(&server, &kinectRGBSource, "Kinect_RGB", "freenect2|RGB|OpenCV");
+
+  if(!openServer.open(&server))
   {
-    std::cout << "no kinectDevice connected or failure opening the default one!" << std::endl;
-    //Aria::exit(5);
-    exit(5);
-  }
-
-  libfreenect2::SyncMultiFrameListener kinectListener(libfreenect2::Frame::Color | libfreenect2::Frame::Ir | libfreenect2::Frame::Depth);
-  libfreenect2::FrameMap kinectFrames;
-
-  kinectDev->setColorFrameListener(&kinectListener);
-  kinectDev->setIrAndDepthFrameListener(&kinectListener);
-  kinectDev->start();
-
-  std::cout << "kinectDevice serial: " << kinectDev->getSerialNumber() << std::endl;
-  std::cout << "kinectDevice firmware: " << kinectDev->getFirmwareVersion() << std::endl;
-
-#if 0
-  kinectDepthSource.open();
-  kinectIRSource.open();
-  kinectRGBSource.open();
-#endif
-
-  /* Run ArNetworking server */
-#if 0
-  if(!simpleOpener.open(&server))
-  {
-    ArLog::log(ArLog::Terse, "\nkinectArVideoServer: Error opening server on port %d!", server.getTcpPort());
-    Aria::exit(2);
-    return 2;
+    std::cout << "error opening ArNetworking server" << std::endl;
+    Aria::exit(5);
+    return 5;
   }
   server.runAsync();
-  ArLog::log(ArLog::Normal, "\nkinectArVideoServer: Server running and ready for client connections on port %d.\n", server.getTcpPort());
-#endif
-  
-  /* Loop reading Kinect frames */
-  while(true)
+  std::cout << "ArNetworking server running on port " << server.getTcpPort() << std::endl;
+
+
+  /* Main loop, capture images from kinect, display, copy to ArVideo sources */
+
+  bool first = true;
+  while(!protonect_shutdown)
   {
-    printf("."); fflush(stdout);
-    kinectListener.waitForNewFrame(kinectFrames);
-    libfreenect2::Frame *rgb = kinectFrames[libfreenect2::Frame::Color];
-    libfreenect2::Frame *ir = kinectFrames[libfreenect2::Frame::Ir];
-    libfreenect2::Frame *depth = kinectFrames[libfreenect2::Frame::Depth];
+    std::cout << "." << std::flush;
+    listener.waitForNewFrame(frames);
+    libfreenect2::Frame *rgb = frames[libfreenect2::Frame::Color];
+//    libfreenect2::Frame *ir = frames[libfreenect2::Frame::Ir];
+    libfreenect2::Frame *depth = frames[libfreenect2::Frame::Depth];
 
-    cv::imshow("rgb", cv::Mat(rgb->height, rgb->width, CV_8UC3, rgb->data));
-    cv::imshow("ir", cv::Mat(ir->height, ir->width, CV_32FC1, ir->data) / 20000.0f);
-    cv::imshow("depth", cv::Mat(depth->height, depth->width, CV_32FC1, depth->data) / 4500.0f);
+    // todo don't recreate Mat objects each time
+    cv::Mat rgbm(rgb->height, rgb->width, CV_8UC3, rgb->data);
+    cv::Mat rgbm_small(rgb->height, rgb->width, CV_8UC3);
+    cv::resize (rgbm, rgbm_small, cv::Size(640, 480));
 
-#if 0
-    // scale them down and provide copies to ArVideo sources 
-    kinectRGBSource.updateVideoDataCopy(cv::Mat(rgb->height, rgb->width, CV_8UC3, rgb->data) / 2000.0);
-    kinectIRSource.updateVideoDataCopy(cv::Mat(ir->height, ir->width, CV_32FC1, ir->data) / 2000.0);
-    kinectDepthSource.updateVideoDataCopy(cv::Mat(depth->height, depth->width, CV_32FC1, depth->data) / 4500.0);
-#endif
-    kinectListener.release(kinectFrames);
+    cv::Mat depthm(depth->height, depth->width, CV_32FC1, depth->data);
+
+    cv::imshow("rgb", rgbm_small);
+//    cv::imshow("ir", cv::Mat(ir->height, ir->width, CV_32FC1, ir->data) / 20000.0f);
+    cv::imshow("depth", depthm / 4500.0f);
+
+    if(!kinectRGBSource.updateVideoDataCopy(rgbm_small, 1, CV_BGR2RGB))
+      std::cout << "Warning error copying rgb data to ArVideo source" << std::endl;
+    if(!kinectDepthSource.updateVideoDataCopy(depthm/4500.0f, 255,
+/*(1/255.0),*/ CV_GRAY2RGB))
+      std::cout << "Warning error copying depth data to ArVideo source" << std::endl;
+  
+    
+    int key = cv::waitKey(1);
+
+    protonect_shutdown = protonect_shutdown || (key > 0 && ((key & 0xFF) == 27)); // shutdown on escape
+
+    listener.release(frames);
+    //libfreenect2::this_thread::sleep_for(libfreenect2::chrono::milliseconds(100));
+
+    if(first)
+    {
+      first = false;
+      cv::moveWindow("rgb",   90, 85); 
+      cv::moveWindow("depth", 90, 599);
+    }
   }
 
-  kinectDev->stop();
-  kinectDev->close();
+  shutdown_app();
 
-
-//  Aria::exit(0);
   return 0;
 }
-
-
-
-
